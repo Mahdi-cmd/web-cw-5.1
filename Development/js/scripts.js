6bef72024470546cebc66465d0907681ba346be5
Mahdi-cmd <66637075+Mahdi-cmd@users.noreply.github.com>
2020-06-27 19:53:43 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,28 +1,42 @@
 // السؤال الأول
 // 1
-
+const name = "Mahdi";
 // 2
-
+let age = 16;
 // 3 and 4
-
+console.log("I am " + name + " and my age is " + age);
 // 5 and 6
-
+age *= 5;
+console.log(age);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+let hobbies =  ["Football", "tennis", "Basketball" ];
 
 // 2
-
+ hobbies.push(16);
+ hobbies.push(16);
+ 
 // 3
+console.log(hobbies.length);
+hobbies.pop();
+ console.log(hobbies);
 
 // 4
-
+ obj = {
+     language: "web",
+     fav: "Python",
+     
+ };
 // 5
+   obj.teacher = "Moudhi"
 
 // 6
+console.log(obj.language);
 
 // 7
+console.log(obj.teacher);
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
